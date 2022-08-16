a30a9ccdac93f99f832575f7abb79494c14af10a
Edgar Sanue <102713836+edgarsanue@users.noreply.github.com>
2022-08-16 17:29:02 -0300

    first push

---
+++ script.js
@@ -0,0 +1,33 @@
+let homePoints = document.getElementById('home-points');
+
+let guestPoints = document.getElementById('guest-points')
+
+function plusH1() {
+    homePoints.textContent++;
+}
+
+function plusH2() {
+    homePoints.textContent++;
+    homePoints.textContent++;
+}
+
+function plusH3() {
+    homePoints.textContent++;
+    homePoints.textContent++;
+    homePoints.textContent++;  
+}
+
+function plusG1() {
+    guestPoints.textContent++;
+}
+
+function plusG2() {
+    guestPoints.textContent++;
+    guestPoints.textContent++;
+}
+
+function plusG3() {
+    guestPoints.textContent++;
+    guestPoints.textContent++;
+    guestPoints.textContent++;
+}
